e0565fddbd832d145b733684609743525c6a0fe0
Paco Navarro <packarbell@gmail.com>
2018-11-08 16:59:53 +0100

    hrttp

--- src/com/pakonat/http/Server.java
+++ src/com/pakonat/http/Server.java
@@ -1,8 +1,11 @@
 package com.pakonat.http;
 
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+
+import org.json.JSONObject;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -12,16 +15,28 @@ public class Server {
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/", new MyHandler());
+        server.createContext("/", new IndexHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
+        readIndex();
     }
 
-    static class MyHandler implements HttpHandler {
+    private static void readIndex() {
+    	FileReader in = new FileReader("/test.txt");
+        BufferedReader br = new BufferedReader(in);
+	}
+
+	static class IndexHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response = "This is the response";
+            String response ="";
             System.out.println(t.getRequestURI());
+
+            if (t.getRequestURI().toString().equals("/data")) {
+            	response = serializeData();
+            } else {            	
+            	response = "This is the response";
+            }
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
@@ -29,4 +44,12 @@ public class Server {
         }
     }
 
+	public static String serializeData() {
+		JSONObject jo = new JSONObject();
+		jo.put("name", "jon doe");
+		jo.put("age", "22");
+		jo.put("city", "chicago");
+		
+		return jo.toString();
+	}
 }
